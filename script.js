a81581b6f27aa18c21f776e75bb5084dcf119cd5
PAOgtsv <ogurtsovpa@gmail.com>
2020-11-27 10:29:23 +0300

    Chages for conflict

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function someFunction(a,b){
     if(a>=b){
 	return "A is much greater... maybe not so much, but still not equal";
-    } else {
+    } else if(a<b) {
 	return "Turns out b is greater... weird";
-    };
+    } else {
+    	return "still existing";
+    }	;
+   
